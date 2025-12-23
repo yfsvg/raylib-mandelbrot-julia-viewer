c888ee4ceb7dcb4a0d59f73872681a1928b8bca1
yfsvg <yifeichengmi@gmail.com>
2025-12-23 11:16:54 -0500

    gurh

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,79 @@
-// The mandelbrot project begins :)
-// Setup raylib, figured out how to actually use github properly lol
-// ken lee
+// Improvements made:
+// -----------------------
+// Logarithmic zoom
+// Moving entire texture instead of recalculating
+// Float -> Double
+// Smart redrawing
+// Double -> long double
+// Local coordinates
 
-// Things to do for this project:
-// 1. Making the mandelbrot implementation actually work
-// 2. Adding a zooming function, and then somehow making the offset work with that
-// 3. Useable ui
+// Optimizations / features to make:
+// Loading screen so that you dont move during calculations
+// Shading
+// Add a number line, mouse pointer coord display
+// Paramaterize z, make Julia set
 
 #include <math.h>
 #include <cstdlib>
 #include <complex>
+#include <vector>
+
 #include "raylib.h"
 #include "raymath.h"
 
-double zoomFactor = 250;
 
-bool movingRightNow = false;
 
-void drawOrigin(float offsetX, float offsetY) {
-    DrawRectangle((GetScreenWidth() / 2 - 1) - offsetX, 0, 2, GetScreenHeight(), RED);
-    DrawRectangle(0, (GetScreenWidth() / 2 - 1) - offsetY, GetScreenWidth(), 2, RED);
+struct SuperVector2 {
+    long double x;
+    long double y;
+};
+
+bool canMove = true;
+long double zoomFactor = 250;
+
+bool usingHeatMapOrNo = false;
+
+void drawIntercepts(long double offsetX, long double offsetY) {
+    DrawRectangle(GetScreenWidth() / 2 - 1 - (float)offsetX, 0, 2, GetScreenHeight(), RED);
+    DrawRectangle(0, GetScreenHeight() / 2 - 1 - (float) offsetY, GetScreenWidth(), 2, RED);
 }
 
+// Apparently the periodicty code doesnt work AT ALL! Be sure to look into ts later
+bool isInMandelbrot(std::complex<long double> c) {
+    // c is the complex parameter/number that is being tested to see if its in the set
+    // z is the current iteration of the mandelbrot result, and z_old is the previous one, used to detect periodicity and such
 
-Vector2 offsetControls(float offsetX, float offsetY) {
-
-    Vector2 returnVal = {offsetX, offsetY};
-    if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) {
-        returnVal.y = offsetY + 1.0;
-        movingRightNow = true;
-    }
-    if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {
-        returnVal.y = offsetY - 1.0;
-        movingRightNow = true;
-    }
-    if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
-        returnVal.x = offsetX - 1.0;
-        movingRightNow = true;
-    }
-    if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
-        returnVal.x = offsetX + 1.0;
-        movingRightNow = true;
-    }
-
-    return returnVal;
-}
-
-bool isInMandelbrot(std::complex<double> c) {
-    std::complex<double> z(0.0, 0.0);
-    // Detecting if the points are in the main cardiod and the second biggest cardiod, useful when its big
-    if ((c.real() + 0.25) * (c.real() + 0.25) + c.imag() * c.imag() < 0.25) {
+    std::complex<long double> z(0.0L, 0.0L);
+    // Figuring out if the thing is in the main cardiod or not
+    if ((c.real() + 0.25L) * (c.real() + 0.25L) + c.imag() * c.imag() < 0.25L) {
         return true;
     }
-    if ((c.real() + 1) * (c.real() + 1) + c.imag() * c.imag() < 0.06) {
+    // Figuring out if its in the second cardiod
+    if ((c.real() + 1.0L) * (c.real() + 1.0L) + c.imag() * c.imag() < 0.06L) {
         return true;
     }
+    // Figuring out if the x val is over 0.5
+    if (c.real() > 0.5L) {
+        return false;
+    }
+    // Figuring out if the y val is over 1.2 (Imaginary)
+    if (std::abs(c.imag()) > 1.2L) {
+        return false;
+    }
+    // These early breaks might be helpful for bigger pictures
 
-    std::complex<double> z_old(0.0, 0.0);
-    // Detecting periodocity (If a number loops between two things over and over again then its in the thing)
+    std::complex<long double> z_old(0.0L, 0.0L);
     int period = 0;
     for (int i = 0; i < 100; i++)
     {
         z = z * z + c;
-        if (std::abs(z) > 2.0)
+        if (std::abs(z) > 2.0L)
         {
             return false;
         }
         if (i == period)
         {
-            if (std::abs(z - z_old) < 1e-10)
+            if (std::abs(z - z_old) < 1e-10L)
             {
                 return true;
             }
@@ -79,58 +84,159 @@ bool isInMandelbrot(std::complex<double> c) {
     return true;
 }
 
-// This draws out all of the points of mandelbrot set one by one! I sure hope I don't regret this implementation.
-// I got a suggestion to use some marching squares thing but that sounds too complicated lololol i'll make it smooth later
-void drawAllPoints(float offsetX, float offsetY) {
-    int screenWidth = GetScreenWidth();
-    int screenHeight = GetScreenHeight();
-    int centerX = screenWidth / 2;
-    int centerY = screenHeight / 2;
-
-    for (int x = screenWidth * -0.5; x <= screenWidth * 0.5; x++) {
-        for (int y = screenHeight * -0.5; y <= screenHeight * 0.5; y++) {
-
-            if (isInMandelbrot(std::complex<double>((x + offsetX) / zoomFactor, (y + offsetY) / zoomFactor))) {
-                DrawRectangle(centerX + x, centerY + y, 1, 1, RED);
+int isInMandlebrotButGiveIterationsToEscape(std::complex<long double> c) {
+    std::complex<long double> z(0.0L, 0.0L);
+    std::complex<long double> z_old(0.0L, 0.0L);
+    int period = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        z = z * z + c;
+        if (std::abs(z) > 2.0L)
+        {
+            return i + 1;
+        }
+        if (i == period)
+        {
+            if (std::abs(z - z_old) < 1e-10L)
+            {
+                return -1;
             }
-
+            z_old = z;
+            period = i;
         }
     }
+    return -1;
 }
 
+// Changed the implementation of the way that the texture is shown, instead of calculating everything
+// Only redraw the texture after each change in perspective
+void renderMandelbrotToTexture(RenderTexture2D mandieSet, long double offsetX, long double offsetY) {
+    BeginTextureMode(mandieSet);
+    ClearBackground(RAYWHITE);
 
-int main(void)
-{
-    Vector2 offset = {0, 0};
-    InitWindow(600, 600, "Mandelbrot Set Viewer");
+    int setWidth = mandieSet.texture.width;
+    int setHeight = mandieSet.texture.height;
+
+    // Calculating the center in complex world using local coordinates
+    long double centerReal = offsetX / zoomFactor;
+    long double centerImag = offsetY / zoomFactor;
+
+    if (usingHeatMapOrNo) {
+        // If the heat map is on, then use the isInMandlebrotButGiveIterationsToEscape function, not implemented yet but this is how i want to do it:
+        // Go through each pixel, just like the nested for loops below
+
+        
+    } else {
+
+        for (int x = setWidth * -0.5; x <= setWidth * 0.5; x++) {
+            for (int y = setHeight * -0.5; y <= setHeight * 0.5; y++) {
+                long double real = centerReal + x / zoomFactor;
+                long double imag = centerImag + y / zoomFactor;
+
+                if (isInMandelbrot(std::complex<long double>(real, imag))) {
+                    DrawRectangle(setWidth / 2 + x, setHeight / 2 + y, 1, 1, RED);
+                }
+
+
+            }
+        }
+
+    }
+
+    EndTextureMode();
+}
+
+SuperVector2 offsetControls(long double offsetX, long double offsetY, RenderTexture2D mandelbrotTexture, SuperVector2 *renderOffset, bool *needsRedraw) {
+    SuperVector2 returnVal = {offsetX, offsetY};
+    if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) {
+        returnVal.y = offsetY + 1.0L;
+    }
+    if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {
+        returnVal.y = offsetY - 1.0L;
+    }
+    if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
+        returnVal.x = offsetX - 1.0L;
+    }
+    if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
+        returnVal.x = offsetX + 1.0L;
+    }
+
+    // Check if any movement key was released
+    if (IsKeyReleased(KEY_DOWN) || IsKeyReleased(KEY_S) ||
+        IsKeyReleased(KEY_UP) || IsKeyReleased(KEY_W) ||
+        IsKeyReleased(KEY_LEFT) || IsKeyReleased(KEY_A) ||
+        IsKeyReleased(KEY_RIGHT) || IsKeyReleased(KEY_D))
+    {
+        *needsRedraw = true;
+    }
+
+    return returnVal;
+}
+
+int main(void) {
+    SuperVector2 offset = {0, 0};
+    SuperVector2 renderOffset = {0, 0};
+    InitWindow(600, 600, "Mandelbrot");
+
+    RenderTexture2D mandelbrotTexture = LoadRenderTexture(600, 600);
+    bool needsRedraw = true;
+
     SetTargetFPS(120);
 
-    while (!WindowShouldClose())
-    {
+    long double previousZoom = zoomFactor;
+
+    while (!WindowShouldClose()) {
+        if (canMove) {
+            SuperVector2 newOffset = offsetControls(offset.x, offset.y, mandelbrotTexture, &renderOffset, &needsRedraw);
+            offset = newOffset;
+
+            if (IsKeyDown(KEY_M)) {
+                zoomFactor += zoomFactor / 50.0L;
+            }
+            if (IsKeyDown(KEY_N)) {
+                zoomFactor -= zoomFactor / 50.0L;
+            }
+            if (IsKeyReleased(KEY_M) || IsKeyReleased(KEY_N)) {
+                if (fabsl(zoomFactor - previousZoom) > 0.01L) {
+                    long double zoomRatio = zoomFactor / previousZoom;
+                    offset.x = offset.x * zoomRatio;
+                    offset.y = offset.y * zoomRatio;
+
+                    needsRedraw = true;
+                    previousZoom = zoomFactor;
+                }
+            }
+        }
+
+        if (needsRedraw) {
+            canMove = false;
+            renderMandelbrotToTexture(mandelbrotTexture, offset.x, offset.y);
+            renderOffset = offset;
+            needsRedraw = false;
+            canMove = true;
+        }
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        
-        offset = offsetControls(offset.x, offset.y);
-        drawAllPoints(offset.x, offset.y);
-        if (IsKeyDown(KEY_M))
-        {
-            zoomFactor += 0.1;
-        }
-        if (IsKeyDown(KEY_N))
-        {
-            zoomFactor -= 0.1;
-        }
-        
-        drawOrigin(offset.x, offset.y);
+        Vector2 texturePos = { (float)(renderOffset.x - offset.x), (float)(renderOffset.y - offset.y) };
+        DrawTextureRec(mandelbrotTexture.texture,
+                        (Rectangle){0, 0, (float)mandelbrotTexture.texture.width, (float)-mandelbrotTexture.texture.height},
+                        texturePos, WHITE);
 
-        
+        drawIntercepts(offset.x, offset.y);
+        DrawFPS(10, 10);
+        DrawText(TextFormat("Zoom: %.1Lf", zoomFactor), 10, 30, 20, DARKGRAY);
+
         EndDrawing();
     }
 
+    UnloadRenderTexture(mandelbrotTexture);
     CloseWindow();
     return 0;
 }
 
-
-// ohhhh Im watching the new Lemino video, somehow I didn't see it until now :(
-// It's the one about the pioneer
+// listening to kid cudi rightnow he's peak
+// My top 3 kanye (objectively correct):
+// 1. Ghost town
+// 2. Family Business
+// 3. Waves
